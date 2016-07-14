df9e9ab85114e16bb83f08b4059c028be6b76d78
Shashank Shekhar <shashankeshav@gmail.com>
2016-07-14 12:11:05 -0700

    correcting the impl for recursive sol to use DP

--- CoinCents/CoinsProblem.py
+++ CoinCents/CoinsProblem.py
@@ -5,8 +5,9 @@ Date: July 12, 2016
 
 import datetime
 
+infy = 9999
+coinSol = [infy] * infy
 
-coinSol = [9999] * 9999
 
 def coinProb(sumLeft):
     if sumLeft == 0:
@@ -15,33 +16,45 @@ def coinProb(sumLeft):
     elif sumLeft < 1:
         return 9999
     else:
-        '''coin_1 = min(coinSol[sumLeft-1], int(coinProb(sumLeft-1)))
-        coin_10 = min(coinSol[sumLeft-10], int(coinProb(sumLeft-10)))
-        coin_30 = min(coinSol[sumLeft-30], int(coinProb(sumLeft-30)))
-        coin_40 = min(coinSol[sumLeft-40], int(coinProb(sumLeft-40)))
-        coins = coinSol[sumLeft] = 1 + min(coin_1, coin_10, coin_30, coin_40)
-        '''
-        coin_1 = min(coinSol[sumLeft-1], int(coinProb(sumLeft - 1)))
-        coin_10 = min(coinSol[sumLeft-10], int(coinProb(sumLeft - 10)))
-        coin_30 = min(coinSol[sumLeft-30], int(coinProb(sumLeft - 30)))
-        coin_40 = min(coinSol[sumLeft-40], int(coinProb(sumLeft - 40)))
-        coins = 1 + min(coin_1, coin_10, coin_30, coin_40)
-        coinSol[sumLeft] = coins
-        #print(coins)
+        # calculation for coin 1
+        if (sumLeft - 1) < 0:
+            use_1 = infy
+        else:
+            use_1 = coinSol[sumLeft - 1]
+        # calculation for coin 10
+        if (sumLeft - 10) < 0:
+            use_10 = infy
+        else:
+            use_10 = coinSol[sumLeft - 10]
+        # calculation for coin 30
+        if (sumLeft - 30) < 0:
+            use_30 = infy
+        else:
+            use_30 = coinSol[sumLeft - 30]
+        # calculation for coin 40
+        if (sumLeft - 40) < 0:
+            use_40 = infy
+        else:
+            use_40 = coinSol[sumLeft - 40]
+        # calculate the number of coins needed
+        coins = 1 + min(use_1, use_10, use_30, use_40)
         return coins
+
+
+def numberOfCoins(sum):
+    for p in range(0, sum+1):
+        coinSol[p] = coinProb(p)
+    return coinSol[sum]
 
 
 def main():
     print("Coin problem for homework assignment")
     a = datetime.datetime.now()
-    for c in range(1, 100):
-        print(str(c) + " : " + str(coinProb(c)))
+    for c in range(1, 1001):
+        print(str(c) + " : " + str(numberOfCoins(c)))
         b = datetime.datetime.now()
         print(b-a)
-    #print("For 52 cents, Solution is : " + str(coinProb(100)))
 
-    #for x in range(999):
-     #   print(coinSol[x])
 
 if __name__ == "__main__":
     main()
